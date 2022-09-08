ebad16f261d2f453c8916ed1bee6ea9fc1584a08
Ettore1024 <ettoreroc@gmail.com>
2022-09-08 17:48:37 +0200

    Functions properly commented + added key in input.json

--- scripts/InputFilePreparation/input_file_preparation.py
+++ scripts/InputFilePreparation/input_file_preparation.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+
+"""
+	The script implements the generation of CAMISIM's input files
+	starting from two files containing interesting information
+	for AMR studies
+"""
+
+__author__ = "Ettore Rocchi"
+
 import csv
 import json
 import sys
@@ -6,7 +16,16 @@ from configparser import ConfigParser
 
 def read_and_store_tsv_input (input_tsv_path):
 	"""
-		Comment here
+		This function reads the input.tsv file and stores the information
+		in a dictionary.
+
+		Parameters:
+			- input_tsv_path: (string) absolute path of input.tsv
+
+		Output:
+			- tsv_columns_dict: (dictionary) each key contains a list
+					    of values, each one associated to a
+					    different input genome
 	"""
 
 	tsv_columns_dict = {
@@ -41,7 +60,15 @@ def read_and_store_tsv_input (input_tsv_path):
 
 def read_and_store_json_input (input_json_path):
 	"""
-		Comment here
+		This function reads the input.json file and stores the information
+		in a dictionary.
+
+		Parameters:
+			- input_json_path: (string) absolute path of input.json
+
+		Output:
+			- json_to_dict: (dictionary) each key is a configuration file
+					setting
 	"""
 
 	with open(input_json_path, 'r') as input_json:
@@ -52,7 +79,18 @@ def read_and_store_json_input (input_json_path):
 
 def wrt_abundance_file (abundance_path, dict_tsv_columns):
 	"""
-		Comment here
+		This function writes the abundance.tsv file starting from the information
+		taken from input.tsv
+
+		Parameters:
+			- abundance_path: (string) absolute path of abundance.tsv
+					  (it doesn't need to exist)
+			- dict_tsv_columns: (dictionary) it contains the information
+					    stored by the read_and_store_tsv_input
+					    function
+
+		Output:
+			No output is returned, but the abundance.tsv file is written
 	"""
 
 	with open(abundance_path, 'w') as abundance_file:
@@ -63,7 +101,18 @@ def wrt_abundance_file (abundance_path, dict_tsv_columns):
 
 def wrt_metadata_file (metadata_path, dict_tsv_columns):
 	"""
-		Comment here
+		This function writes the metadata.tsv file starting from the information
+		taken from input.tsv
+
+		Parameters:
+			- metadata_path: (string) absolute path of metadata.tsv
+					 (it doesn't need to exist)
+			- dict_tsv_columns: (dictionary) it contains the information
+					    stored by the read_and_store_tsv_input
+					    function
+
+		Output:
+			No output is returned, but the metadata.tsv file is written
 	"""
 
 	with open(metadata_path, 'w') as metadata_file:
@@ -78,7 +127,18 @@ def wrt_metadata_file (metadata_path, dict_tsv_columns):
 
 def wrt_genome_to_id_file (genome_to_id_path, dict_tsv_columns, simul_dir):
 	"""
-		Comment here
+		This function writes the genome_to_id.tsv file starting from the information
+		taken from input.tsv
+
+		Parameters:
+			- genome_to_id_path: (string) absolute path of genome_to_id.tsv
+					     (it doesn't need to exist)
+			- dict_tsv_columns: (dictionary) it contains the information
+					    stored by the read_and_store_tsv_input
+					    function
+
+		Output:
+			No output is returned, but the genome_to_id.tsv file is written
 	"""
 
 	with open(genome_to_id_path, 'w') as genome_to_id_file:
@@ -90,7 +150,29 @@ def wrt_genome_to_id_file (genome_to_id_path, dict_tsv_columns, simul_dir):
 
 def configuration_file_definition (configuration_file):
 	"""
-		Comment here
+		This function defines the sections and the parameters required by CAMISIM
+		to launch the simulation.
+
+		Some of the parameters are already set here (some of them to help the user,
+		when the parameter is not crucial, others because the considered AMR study
+		requires precise settings).
+
+		What's new?
+			W.r.t the original version of CAMISIM there are three new parameters:
+			- path_to_abundance_file: (string) absolute path of abundance.tsv
+			- equally_distributed_strains: (bool) it decides if the number of generated
+						       strains will be equally distributed among
+						       the original genomes or not
+			- input_genomes_to_zero: (bool) it decides if the input genomes' abundances
+						 will be totally re-distributed among the strains
+						 or not
+
+		Parameters:
+			- configuration_file: (ConfigParser) this object will be updated
+					      inside this function
+
+		Output:
+			No output is returned, but the ConfigParser object is updated
 	"""
 
 	configuration_file['Main'] = {
@@ -134,7 +216,7 @@ def configuration_file_definition (configuration_file):
                 'ratio': '1',
                 'mode': 'known_distribution',
                 'equally_distributed_strains': '',
-                'input_genomes_to_zero': 'True',
+                'input_genomes_to_zero': '',
                 'log_mu': '1',
                 'log_sigma': '2',
                 'gauss_mu': '1',
@@ -143,9 +225,27 @@ def configuration_file_definition (configuration_file):
 	}
 
 
-def configuration_file_settings (configuration_file, dict_json, dict_of_columns, abundance_path, metadata_path, genome_to_id_path, simulation_directory):
+def configuration_file_settings (configuration_file, dict_json, dict_tsv_columns, abundance_path, metadata_path, genome_to_id_path, simulation_directory):
 	"""
-		Comment here
+		This function updates all the empty parameters of the configuration file,
+		using the information taken from input.json and input.tsv
+
+		Parameters:
+			- configuration_file: (ConfigParser) this object will be updated
+					      inside this function
+			- dict_json: (dictionary) it contains the information stored by the
+				     read_and_store_json_input function
+			- dict_tsv_columns: (dictionary) it contains the information stored by
+					    the read_and_store_tsv_input function
+			- abundance_path: (string) absolute path of abundance.tsv
+			- metadata_path: (string) absolute path of metadata.tsv
+			- genome_to_id_path: (string) absolute path of genome_to_id.tsv
+			- simulation_directory: (string) absolute path of the directory of the
+						simulation
+
+		Output:
+			No output is returned, but the settings of the CongfigParser object
+			are updated
 	"""
 
 	configuration_file.set('Main', 'max_processor', dict_json['max_processor'])
@@ -159,37 +259,59 @@ def configuration_file_settings (configuration_file, dict_json, dict_of_columns,
 	configuration_file.set('community0', 'id_to_genome_file', genome_to_id_path)
 	configuration_file.set('community0', 'path_to_abundance_file', abundance_path)
 	configuration_file.set('community0', 'genomes_total', dict_json['genomes_total'])
-	configuration_file.set('community0', 'num_real_genomes', str(len(dict_of_columns['genome_IDs'])))
+	configuration_file.set('community0', 'num_real_genomes', str(len(dict_tsv_columns['genome_IDs'])))
 	configuration_file.set('community0', 'equally_distributed_strains', dict_json['equally_distributed_strains'])
+	configuration_file.set('community0', 'input_genomes_to_zero', dict_json['input_genomes_to_zero'])
 
 
 def wrt_configuration_file (configuration_file_path, configuration_file):
 	"""
-		Comment here
+		This function writes the config.ini file starting from the information
+		taken from input.json
+
+		Parameters:
+			- configuration_file_path: (string) absolute path of config.ini
+						   (it doesn't need to exist)
+			- configuration_file: (ConfigParser) this object contains all the
+					      (already set) parameters which constitute the
+					      configuration file
+
+                Output:
+                        No output is returned, but the config.ini file is written
 	"""
 
 	with open(configuration_file_path, 'w') as config_file:
 		configuration_file.write(config_file)
 
 
-def wrt_genomes_info_file (genomes_info_path, dict_of_columns, input_tsv_path):
+def wrt_genomes_info_file (genomes_info_path, dict_tsv_columns, input_tsv_path):
 	"""
-		Comment here
+		This function writes the genomes_info.json file starting from the information
+		taken from input.tsv
+
+		Parameters:
+			- genomes_info_path: (string) absolute path of genomes_info.json
+					     (it doesn't need to exist)
+			- dict_tsv_columns: (dictionary) it contains the information
+					    stored by the read_and_store_tsv_input function
+
+                Output:
+                        No output is returned, but the genomes_info.json file is written
 	"""
 
 	dict_from_tsv_info = {}
-	for index in range(len(dict_of_columns['genome_IDs'])):
-		dict_from_tsv_info[dict_of_columns['genome_IDs'][index]] = {}
+	for index in range(len(dict_tsv_columns['genome_IDs'])):
+		dict_from_tsv_info[dict_tsv_columns['genome_IDs'][index]] = {}
 
 	with open(input_tsv_path, 'r') as input_tsv:
 		input_tsv_reader = csv.DictReader(input_tsv, delimiter = '\t')
 		i = 0
 		for row in input_tsv_reader:
-			dict_from_tsv_info[dict_of_columns['genome_IDs'][i]].update(dict(row))
+			dict_from_tsv_info[dict_tsv_columns['genome_IDs'][i]].update(dict(row))
 			i += 1
 
 	keys_list = ['Genome_ID', 'Novelty_category']
-	for index in range(len(dict_of_columns['genome_IDs'])):
+	for index in range(len(dict_tsv_columns['genome_IDs'])):
 		for key in keys_list:
 			try:
 				del dict_from_tsv_info[dict_of_columns['genome_IDs'][index]][key]
@@ -202,7 +324,27 @@ def wrt_genomes_info_file (genomes_info_path, dict_of_columns, input_tsv_path):
 
 def amr_pipeline (directory_of_simulation):
 	"""
-		Comment here
+		This function gathers all the above-defined functions and for this reason
+		it represents the structure of the pipeline used for AMR studies.
+
+		Parameters:
+			- directory_of_simulation: (string) the absolute path for the directory of
+						   simulation (containing input.tsv and input.json)
+
+		Output:
+			No output is returned, but five files are created:
+				- abundance.tsv: file containing genome IDs and abundances
+						 (no header)
+				- metadata.tsv: file containing genome IDs, OTUs, NCBI IDs,
+						novelty category (with header)
+				- genome_to_id.tsv: file containing genome IDs and absolute paths
+						    to their fasta file (no header)
+				- config.ini: file with the settings required by CAMISIM to launch
+					      the simulation
+				- genomes_info.json: file with useful information about the original
+						     genomes for AMR studies (e.g. Genome length,
+						     antibiotic and associated phenotype [resistant/
+						     susceptible], PATRIC ID)
 	"""
 
 	file_path_input_tsv = directory_of_simulation + '/input.tsv'
